9e42c1a1feecb189a26e5a40e83165723866d46a
Emilio <thecraftedgem@tuta.io>
2019-04-09 17:49:40 -0600

    looking better still not happy with how big the match works

--- anagram/anagram.exs
+++ anagram/anagram.exs
@@ -1,14 +1,8 @@
 defmodule Anagram do
-  @doc """
-  Returns all candidates that are anagrams of, but not equal to, 'base'.
-  """
-  @spec match(String.t(), [String.t()]) :: [String.t()]
-  def match(base, candidates), do: for x <- candidates, (fingerprint(x) == fingerprint(base)) && (String.downcase(base) != String.downcase(x)), do: x
 
-  defp fingerprint(base) do
-    String.downcase(base)
-    |> String.to_charlist
-    |> Enum.sort
-  end
+  def match(base, candidates), do: for x <- candidates,
+    (fingerprint(x) == fingerprint(base)) && (String.downcase(base) != String.downcase(x)), do: x
+
+  defp fingerprint(base), do: String.downcase(base) |> String.to_charlist |> Enum.sort
 
 end
