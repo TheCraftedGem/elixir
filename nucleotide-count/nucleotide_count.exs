a096abada458c9a2e8a64b8afae7d2568ac2a095
Emilio <thecraftedgem@tuta.io>
2019-09-05 15:07:43 -0600

    Refactored

--- nucleotide-count/nucleotide_count.exs
+++ nucleotide-count/nucleotide_count.exs
@@ -1,20 +1,14 @@
 defmodule NucleotideCount do
   @nucleotides [?A, ?C, ?G, ?T]
 
-  def count(strand, nucleotide) do
+  defp initial_histogram(), do: %{?A => 0, ?T => 0, ?C => 0, ?G => 0}
+
+  def count(strand, nucleotide), do:
     Enum.count(strand, fn e -> e == nucleotide end)
-  end
 
-  defp initial_histogram() do
-    %{?A => 0, ?T => 0, ?C => 0, ?G => 0}
-  end
+  def histogram(strand), do:
+    Enum.reduce(strand, initial_histogram(), &update_histogram/2)
 
-  def histogram(strand) do
-    histogram = initial_histogram()
-    Enum.reduce(strand, histogram, &update_histogram/2)
-  end
-
-  def update_histogram(nucleotide, histogram) do
+  def update_histogram(nucleotide, histogram), do:
     Map.update!(histogram, nucleotide, &(&1 + 1))
-  end
 end
